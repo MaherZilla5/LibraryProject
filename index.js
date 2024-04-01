6d9e78ed5a72490ef9a891e8a2ec8233471e4019
Matthew Maher <matt@kmaher.com>
2024-03-31 22:36:39 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function TestFunction(){
+    return (
+    <div>
+        <h1 id="testElement">This is a test</h1>
+    </div>)
+}
+
+
+ReactDOM.render(<TestFunction/>, document.getElementById('root'));
